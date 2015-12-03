0cfe15bebc42d09755d6bd4046f18fd87e6ccf22
tmtwd <t.williams.im@gmail.com>
2015-12-03 00:37:44 -0500

    pushed a channels to children components to experiment

--- src/cljs/tic_tac_toe/core.cljs
+++ src/cljs/tic_tac_toe/core.cljs
@@ -7,6 +7,7 @@
 
 (enable-console-print!)
 
+
 (defonce app-state (atom {:text "Hello Chestnut!"}))
 
 (defn x-cord
@@ -17,11 +18,28 @@
   [pair]
   (get-in pair [1]))
 
+(defn eq-pos? [pos1 pos2]
+  (and (= (x-cord pos1) (x-cord pos2))
+       (= (y-cord pos1) (y-cord pos2))))
 
-(defcomponent cell [data owner {:keys [pos]}]
+(defcomponent cell [data owner {:keys [pos rand-chan]}]
   (init-state [_]
               {:clicked false})
-  (render-state [this {:keys [info]}]
+  (will-mount [_]
+              (if (om/get-state owner :fun-chan)
+                (let [fun-chan (om/get-state owner :fun-chan)]
+                  (go (loop []
+                        (let [my-pos (<! fun-chan)]
+                          (js/console.log "the pos received in is  in cell" (x-cord my-pos) (y-cord my-pos))
+                          (js/console.log "the current pos is" (x-cord pos) (y-cord pos))
+                          (if (eq-pos? pos my-pos)
+                            (om/update-state! owner :clicked not))
+                          (recur)))))
+                (js/console.log "um not there")))
+  (render-state [this {:keys [info fun-chan]}]
+          (if fun-chan
+            (js/console.log "it existst")
+            (js/console.log "it doesn't exist"))
           (dom/td #js {:className "cell"
                        :id (if (= true (om/get-state owner :clicked))
                              "light-blue"
@@ -46,35 +64,44 @@
     (init-state [_]
       {:x-cell (range 3), :y-cell (range 3)})
     (render-state [_ state]
+                  (if (:fun-chan state)
+                    (js/console.log "yolo")
+                    (js/console.log "hehe"))
       (apply dom/table #js {:id "chess-board"}
         (for [y (:x-cell state)]
           (apply dom/tr nil
                  (for [x (:y-cell state)]
                    (om/build cell (cell-col x y)
                              {:opts {:pos [x y]}
-                              :init-state {:info (:info state)}})))))))
+                              :init-state {:info (:info state) :fun-chan (when (and (= 0 y)(= 0 x))(:fun-chan state))}})))))))
 
 (defcomponent main-view
   [data owner]
   (init-state [_]
     {:info (chan)
-     :req-chan     (chan)
+     :fun-chan     (chan)
      :pub-chan     (chan)})
 
   (will-mount [_]
-      (let [info (om/get-state owner :info)]
+      (let [info (om/get-state owner :info)
+            fun-chan (om/get-state owner :fun-chan)]
         (go (loop []
               (let [pos (<! info)]
                 (js/console.log "the cords are " (x-cord pos) ", " (y-cord pos))
+                (om/set-state! owner :pos pos)
+                (js/console.log "the cur pos is" (om/get-state owner :pos))
+                (put! fun-chan pos)
                 (recur))))))
 
-  (render-state [_ {:keys [info]}]
+  (render-state [_ {:keys [info fun-chan]}]
                 (when info
                   (js/console.log "info tnsieraotn"))
     (dom/div nil
+        (dom/h1 nil "lol world")
         (om/build board nil {:init-state {:info info}})
-        (om/build board nil {:init-state {:info info}})
-        (om/build board nil {:init-state {:info info}}))))
+        (om/build board nil {:init-state {:info info :fun-chan fun-chan}})
+        (om/build board nil {:init-state {:info info :fun-chan fun-chan}})
+        (dom/button #js {:onClick #(js/console.log "yo")} "click me"))))
 
 (defcomponent my-test
   [data owner]
